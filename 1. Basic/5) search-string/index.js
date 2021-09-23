89bbfd08cf20c58cbf288f8d6d5afcca606c1751
ShiroKaifat <89072081+ShiroKaifat@users.noreply.github.com>
2021-09-23 16:25:34 +0300

    Задание 5

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,5 +1,15 @@
 function searchString(obj) {
-  // Напиши свой код здесь
+    let str = '?';
+    const newArr = Object.entries(obj);
+
+    newArr.forEach(value => {
+
+        if (value[1] !== null && value[1] !== undefined && value[1] !== '') {
+            str += `${value[0]}=${value[1]}&`;
+        }
+    });
+    str = str.slice(0, -1);
+    return str;
 }
 
 window.searchString = searchString;
