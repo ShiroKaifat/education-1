fcc9d5cc0b1f9433f3cb2ed53923f86a4200d24c
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-09-29 16:43:50 +0300

    Задание 3

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,5 +1,17 @@
 function isEqual(a, b) {
-  // Напиши свой код здесь
+  try {
+    let count = 0;
+    Object.entries(a).map(value1 => {
+      Object.entries(b).map(value2 => {
+        if (value2[0] === value1[0] && value2[1] === value1[1]){
+          count++;
+        }
+      });
+    });
+    return (count === Object.values(a).length && count === Object.values(b).length);
+  } catch (err) {
+    return false;
+  }
 }
 
 window.isEqual = isEqual;
