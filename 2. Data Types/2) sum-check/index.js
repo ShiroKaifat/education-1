f342817797c8697ebbbcb65afe3f73f94aaf75fb
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-09-29 11:38:08 +0300

    Задание 2

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,5 +1,11 @@
 function sumCheck(a, b) {
-  // Напиши свой код здесь
+    if (Number.isNaN(a) || Number.isNaN(b)) {
+        return NaN;
+    } else if (!(Number.isInteger(a) && Number.isInteger(b))){
+        throw new Error('Wrong arguments type!');
+    } else {
+        return a + b;
+    }
 }
 
 window.sumCheck = sumCheck;
