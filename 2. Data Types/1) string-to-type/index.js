b9f3e80b813792447bfa86a852611e875db0abb9
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-09-29 09:27:15 +0300

    Задание 1

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,6 +1,15 @@
 function stringToType(str) {
-  // Напиши свой код здесь
-};
+    try {
+        return JSON.parse(str);
+    } catch (err) {
+
+        if (str === 'undefined') {
+            return undefined;
+        } else {
+            return str;
+        }
+    }
+}
 
 window.stringToType = stringToType;
 
