09491d9d1a88f1facb3720ed9bc6c56a835c45d5
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-09-30 16:22:31 +0300

    Задание 2

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,10 +1,9 @@
 function findPair(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr. length - arr.filter(test => test !== arr[i]).length === 2){
-            return arr[i];
+    return arr.find(value => {
+        if (arr. length - arr.filter(test => test !== value).length === 2) {
+            return value;
         }
-    }
-   return null;
+    }) || null;
 }
 
 window.findPair = findPair;
