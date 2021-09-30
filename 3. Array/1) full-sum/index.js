d0f238db90dd09630c61178e565cf5f3477a697a
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-09-30 13:38:56 +0300

    Задание 1

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,11 +1,10 @@
 function fullSum(...args) {
     return args.reduce((acc, cur) => {
         if (Number.isInteger(cur) || Number.isNaN(cur)) {
-            acc += cur;
+            return acc + cur;
         } else {
             throw  new Error("Wrong Argument Type");
         }
-        return acc;
     }, 0);
 }
 
