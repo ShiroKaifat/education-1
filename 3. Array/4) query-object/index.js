5118ba4c83b47f7cb677cdc56309be8ba9106c32
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-10-01 12:10:38 +0300

    Задание 4

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,6 +1,15 @@
 function queryToObject(query) {
-  // Напиши свой код здесь
-};
+    return  query.slice(1).replace(/=/g, ':').split('&').reduce((acc, cur) => {
+        try {
+            if (!['', '?'].includes(cur)) {
+                acc[cur.slice(0, cur.indexOf(':'))] = JSON.parse(cur.slice(cur.indexOf(':') + 1));
+            }
+        } catch {
+            acc[cur.slice(0, cur.indexOf(':'))] = cur.slice(cur.indexOf(':') + 1);
+        }
+        return acc;
+    }, {});
+}
 
 window.queryToObject = queryToObject;
 
