409ada18cebc62583e6d75aab6a9eb340ae75f48
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-10-04 16:11:27 +0300

    Задание 2

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,5 +1,20 @@
 function xhrGet(url) {
-  // Напиши свой код здесь
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.setRequestHeader('Content-Type','application/json');
+        xhr.send();
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                try {
+                    resolve(JSON.parse(xhr.response));
+                } catch (err) {
+                    resolve(xhr.response);
+                }
+            }
+            reject(JSON.parse(xhr.response));
+        }
+    });
 }
 
 window.xhrGet = xhrGet;
