602fad711f497647adf1836ad5b7286d27c5ba04
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-10-06 17:26:43 +0300

    Задание 6

--- 5. Others/6) row-zero/index.js
+++ 5. Others/6) row-zero/index.js
@@ -1,5 +1,19 @@
 function rowZero(str) {
-  // Напиши свой код здесь
+    if (str.includes('0')) {
+        let countZero = [];
+        let count = 0;
+
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === '0') {
+                count++;
+            } else {
+                count = 0;
+            }
+            countZero.push(count);
+        }
+        return Math.max.apply(null, countZero);
+    }
+    return 0;
 }
 
 window.rowZero = rowZero;
