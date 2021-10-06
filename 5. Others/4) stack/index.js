b62723be37c6a81d44e9bc5d8cd9f1143dd5345f
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-10-06 15:57:57 +0300

    Задание 4

--- 5. Others/4) stack/index.js
+++ 5. Others/4) stack/index.js
@@ -1,24 +1,28 @@
-/*
-  Задача
-  Написать стак стек в котором есть следующие методы
-  push - добавляет элимент в конец стека
-  pop - возвращяет удаляет последний элемент из стека, если стек уже пустой генерирует ошибку с ссобщением: "Stack is empty!"
-  isEmpty - проверяет является ли стек пустым
-  print - выводит стек в формате
-  И имеет свойство:
-  size - количество элиментов в списке
-  Пример кода
-  const stack = new Stack();
-  stack.push(1);
-  stack.print() // [1]
-  stack.pop().isEmpty(); // true
-  stack.pop(); // error
-  stack.push(2).push(3).pop()
-  stack.push(10).push(14).pop().print() // "[2, 10]"
- */
-
 class Stack {
-  // Напиши свой код здесь
+  constructor() {
+      this.stack = [];
+      this.size = this.stack.length;
+  }
+  push(item) {
+      this.stack.push(item);
+      this.size = this.stack.length;
+      return this;
+  }
+  pop () {
+      if (this.stack.length === 0) {
+          throw new Error('Stack is empty!');
+      } else {
+          this.stack.pop();
+          this.size = this.stack.length;
+          return this;
+      }
+  }
+  isEmpty () {
+      return this.stack.length === 0;
+  }
+  print () {
+      return `[${this.stack.slice().join(', ')}]`;
+  }
 }
 
 window.Stack = Stack;
