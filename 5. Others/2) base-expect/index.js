b5bd7d13887d0e289f2894646c154ada55a13c7f
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-10-06 09:01:26 +0300

    Задание 2

--- 5. Others/2) base-expect/index.js
+++ 5. Others/2) base-expect/index.js
@@ -1,24 +1,13 @@
-/*
-  Задача написать функцию baseExpect которая будет работать следующим образом
-  Пример
-  baseExpect(4).toBe(4) === true
-  baseExpect(5).toBe(4) === false
-  baseExpect(5).toBe.not(4) === true
-  baseExpect(5).toBe.not(5) === false
-  Примечание: Должно работать только с числами
- */
 function baseExpect(a) {
-    if(Number.isInteger(a)){
-        return a;
+    const toBe = function (b) {
+        return b === a;
     }
-}
-Object.prototype.toBe = function (b) {
-    return b === baseExpect(4);
-}
-Object.prototype.not = function (c) {
-    return c !== baseExpect(4);
+    toBe['not'] = function (c) {
+        return c !== a;
+    }
+    return {toBe};
 }
 
 window.baseExpect = baseExpect;
 
-export default baseExpect.toBe.not;
+export default baseExpect;
