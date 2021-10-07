2d7e70f346d1f8241a80d86f204773045acc7946
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-10-07 09:26:45 +0300

    Задание 7

--- 5. Others/7) is-equal-advances/index.js
+++ 5. Others/7) is-equal-advances/index.js
@@ -1,5 +1,17 @@
 function isEqualAdvances(a, b) {
-  // Напиши свой код здесь
+    try {
+        let count = 0;
+        Object.entries(a).map(value1 => {
+            Object.entries(b).map(value2 => {
+                if (value2[0] === value1[0] && JSON.stringify( value2[1]) === JSON.stringify( value1[1])){
+                    count++;
+                }
+            });
+        });
+        return (count === Object.values(a).length && count === Object.values(b).length);
+    } catch (err) {
+        return false;
+    }
 }
 
 window.isEqualAdvances = isEqualAdvances;
