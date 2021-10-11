467e0d7b54f55e58fe164aa54678e0ad2e2b92db
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-10-11 10:55:23 +0300

    Задание 5

--- 5. Others/5) token-service/index.js
+++ 5. Others/5) token-service/index.js
@@ -2,28 +2,33 @@ class TokenService {
     constructor() {
         this.cbs = [];
     }
+
     subscribe(callback) {
         this.cbs.push(callback);
     }
-    setToken(token) {
 
-        if (!token) {
-            this.removeToken();
-        }else {
+    setToken(token) {
+        if (token) {
 
             if (this.getToken() !== token) {
                 localStorage.setItem('_token_', token);
                 this.fireChangeEvent(token);
+
+        } else {
+                this.removeToken();
             }
         }
     }
+
     removeToken() {
         localStorage.removeItem('_token_');
         this.fireChangeEvent(null);
     }
+
     getToken() {
         return localStorage.getItem('_token_') || null;
     }
+
     fireChangeEvent(token) {
         this.cbs.forEach(cb => cb(token));
     }
