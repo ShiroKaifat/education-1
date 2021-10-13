3564f0e3e62dd3d53a20c1434badc1fc3030fbce
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-10-13 11:28:19 +0300

    Задание 7

--- 5. Others/7) is-equal-advances/index.js
+++ 5. Others/7) is-equal-advances/index.js
@@ -1,8 +1,11 @@
 function isEqualAdvances(a, b) {
     try {
         let count = 0;
+
         Object.entries(a).map(value1 => {
+
             Object.entries(b).map(value2 => {
+
                 if (value2[0] === value1[0] && JSON.stringify( value2[1]) === JSON.stringify( value1[1])){
                     count++;
                 }
