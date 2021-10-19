adfe88a37c732b61f81bbfb2df371874cdc4cc8a
ShiroKaifat <kachalov.exceedteam@gmail.com>
2021-10-19 13:37:42 +0300

    vanilla_todo

--- vanila_todo/script.js
+++ vanila_todo/script.js
@@ -12,36 +12,50 @@ const updateValue = (event) => {
     valueInput = event.target.value;
 }
 
-const onClickButton = (value) => {
-    allTasks.push(value);
+const onClickButton = () => {
+    allTasks.push({
+        text: valueInput,
+        isCheck: false,
+        color: document.getElementById('color-task').value
+    });
     valueInput = 'empty';
     input.value = '';
+    render();
+}
+
+const onChangeCheckbox = (index) => {
+    allTasks[index].isCheck = !allTasks[index].isCheck;
     render();
 }
 
 const render = () => {
     const content = document.getElementById('content');
     const addButton = document.getElementById(`addButton`);
-
-    if (allTasks.length % 2 === 0) {
-        addButton.onclick =  () =>  onClickButton(valueInput.toLowerCase());
-    } else {
-        addButton.onclick =  () =>  onClickButton(valueInput.toUpperCase());
-    }
+    addButton.onclick =  () =>  onClickButton();
 
     while (content.firstChild) {
         content.removeChild(content.firstChild);
     }
 
-    allTasks.map(item => {
+    allTasks.map((item, index) => {
        const container = document.createElement('div');
+       container.id = `task-${index}`;
+       container.className = 'task-container';
+       const divForCheck = document.createElement('div');
+       divForCheck.className = 'divForCheck';
+       const checkbox = document.createElement('input');
+       checkbox.type = 'checkbox';
+       checkbox.checked = item.isCheck;
+       checkbox.onchange = () => onChangeCheckbox(index);
+       checkbox.className = item.isCheck ? 'text-task done-text' : 'text-task';
+       divForCheck.appendChild(checkbox);
+       container.appendChild(divForCheck);
        const text = document.createElement('p');
-
-       if (item.includes('555')) {
-           item = item.replace(/555/g, '---');
-       }
-       text.innerText = item;
+       text.innerText = item.text;
+       text.className = item.isCheck ? 'text-task done-text' : 'text-task';
        container.appendChild(text);
        content.appendChild(container);
+       divForCheck.style.background = item.color;
+       text.style.background = item.color;
     });
 }
